63ca84048dff2dff87c40c54a71ff78404d50370
rbMill <lorenzomillano67@gmail.com>
2022-04-03 15:31:07 +0800

    Sherona

--- projects/kivy/System Input Sequencer/main.py
+++ projects/kivy/System Input Sequencer/main.py
@@ -4,6 +4,7 @@ from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.config import Config
 Config.set('graphics','resizable','1')
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 from kivy.lang.builder import Builder
 from kivy.clock import Clock
 
@@ -13,27 +14,27 @@ class UI(AnchorLayout):
         self.mouseEvent = None
         self.kBoard = Window.request_keyboard(self.kBoard_on_close,self,'text')
         self.kBoard.bind(on_key_down=self.kBoard_on_use,on_key_up=self.kBoard_on_use)
-        print(ScrollView,dir(ScrollView))
     def kBoard_on_use(self,kb=None,key=None,text=None,modifiers=None):
         args = (kb,key,text,modifiers)
-        # print(args[1:])
         if key[1] == 'spacebar':
             print('space')
     def kBoard_not_use(self):
         pass
+
+    def on_motion
+
+
 
     def kBoard_on_close(self):
         self.kBoard.unbind
 
     def on_CLD(self,widg,parent=None):
         if self.mouseEvent == None:
-            print(self.ids)
             if parent == None:
                 parent = widg.parent
             def on_hold_m(args):
                 Mpos = Window.mouse_pos
                 d_x,d_y = round((Mpos[0]-parent.x)/(parent.width),3),round((Mpos[1]-parent.y)/(parent.height),3)
-                print(d_x,d_y,dir(parent))
                 widg.text = str([d_x,d_y]) + str(Mpos)
             self.mouseEvent = Clock.schedule_interval(on_hold_m,0.001)
         else:
