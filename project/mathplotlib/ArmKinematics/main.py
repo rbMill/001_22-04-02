8e6e84ad68bce9bb2deac6143903aada8d82c450
rbMill <lorenzomillano67@gmail.com>
2022-04-06 14:58:26 +0800

    oregairu

--- project/mathplotlib/ArmKinematics/main.py
+++ project/mathplotlib/ArmKinematics/main.py
@@ -3,12 +3,20 @@ import numpy as np
 import math
 
 x,y = [],[]
-for i in range(360*1):
-        x.append(math.cos(i*math.pi/180)*math.sin(i/math.pi))
-        y.append(math.sin(i*math.pi/180)*math.cos(i/math.pi))
-
-
 fig, ax = plt.subplots()
-ax.plot(x, y)
+a = 2
+ax.set_xlim([-1.5, 1.5])
+ax.set_ylim([-1.5, 1.5])
+for i in range(360*1):
+    i = i*math.pi/180
+    x.append(math.cos(i)*i/360)
+    y.append(math.sin(i)*i/360)
+    if len(x) >= 22:
+        x = x[-22:]
+    if len(y) >= 22:
+        y = y[-22:]
+    ax.plot(x, y,'black')
+    plt.pause(0.001)
 ax.set_aspect('equal')
+
 plt.show()
