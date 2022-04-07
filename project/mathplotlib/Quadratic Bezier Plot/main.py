d6b40354616232217cd93075d656bfe2255c5f79
rbMill <lorenzomillano67@gmail.com>
2022-04-07 23:02:40 +0800

    moe

--- project/mathplotlib/Quadratic Bezier Plot/main.py
+++ project/mathplotlib/Quadratic Bezier Plot/main.py
@@ -2,52 +2,63 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-fig, ax = plt.subplots()
+if False | False:
+    print('yee?')
+class Bezier:
+    def __init__(self):
+        fig, self.ax = plt.subplots()
 
-a = 2
-
-ofst = 0.2
-
-xlim = [0-ofst,1+ofst]
-ylim = [0-ofst,1+ofst]
-
-c = 4/3
-
-px,py = [0,0,1],[0,1,1]
-
-res = 50
-
-trajx = np.linspace(px[0],px[1],res)
-trajy = np.linspace(py[0],py[1],res)
-
-trajx1 = np.linspace(px[1],px[2],res)
-trajy1 = np.linspace(py[1],py[2],res)
-
-ax.set_xlim(xlim)
-ax.set_ylim(ylim)
-ax.set_aspect('equal')
-
-# plotting
-ax.plot(px,py,'Black')
-
-_x,_y = px[0],py[0]
-_x1,_y1 = px[1],py[1]
+    def myBezier(self,p1=None,p2=None,p3=None):
+        if p1 == None or p2 == None or p3 == None:
+            px,py = [0,0,1],[0,1,1]
+        else:
+            px,py = [p1[0],p2[0],p3[0]],[p1[1],p2[1],p3[1]]
 
 
-nx,ny = _x,_y
+        ofst = 0.2
+        xlim = [0-ofst,1+ofst]
+        ylim = [0-ofst,1+ofst]
+        res = 50
 
-for i,(x,y,x1,y1) in enumerate(zip(trajx,trajy,trajx1,trajy1)):
-    ax.plot([_x,x],[_y,y],'red')
-    _x,_y = x,y
+        trajx = np.linspace(px[0],px[1],res)
+        trajy = np.linspace(py[0],py[1],res)
 
-    ax.plot([_x1,x1],[_y1,y1],'blue')
-    _x1, _y1 = x1, y1
+        trajx1 = np.linspace(px[1],px[2],res)
+        trajy1 = np.linspace(py[1],py[2],res)
 
-    prc = i/res
-    dx = x + (x1-x)*prc
-    dy = y + (y1-y)*prc
-    ax.plot([nx, dx], [ny, dy], 'green')
-    nx,ny = dx, dy
+        self.ax.set_xlim(xlim)
+        self.ax.set_ylim(ylim)
+        self.ax.set_aspect('equal')
 
-    plt.pause(0.001)
+        # plotting
+        self.ax.plot(px,py,'Black')
+
+        _x,_y = px[0],py[0]
+        _x1,_y1 = px[1],py[1]
+
+
+        nx,ny = _x,_y
+
+        for i,(x,y,x1,y1) in enumerate(zip(trajx,trajy,trajx1,trajy1)):
+            self.ax.plot([_x,x],[_y,y],'red')
+            _x,_y = x,y
+
+            self.ax.plot([_x1,x1],[_y1,y1],'blue')
+            _x1, _y1 = x1, y1
+
+            prc = i/res
+            dx = x + (x1-x)*prc
+            dy = y + (y1-y)*prc
+            self.ax.plot([nx, dx], [ny, dy], 'green')
+            nx,ny = dx, dy
+
+            plt.pause(0.001)
+
+bz = Bezier()
+bz.myBezier([1,1],[1,0],[0,0])
+bz.myBezier([1,1],[0,1],[0,0])
+bz.myBezier([1,1],[0,0],[1,0])
+bz.myBezier([1,1],[0,0],[0,1])
+bz.myBezier([1,0],[1,0],[0,1])
+bz.myBezier([1,0],[0,1],[0,1])
 plt.show()
