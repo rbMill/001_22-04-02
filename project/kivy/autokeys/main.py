807606f59f88ce58779f1f2d48855211563c295a
rbMill <lorenzomillano67@gmail.com>
2022-04-23 14:46:17 +0800

    shi shi gami

--- project/kivy/autokeys/main.py
+++ project/kivy/autokeys/main.py
@@ -1,10 +1,14 @@
 import regex
 import math
+
+from pynput.keyboard import Controller as Kcontrol,Key
+from pynput.mouse import Controller as Mcontrol,Button as MButton
+
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.properties import NumericProperty
-from kivy.graphics import Canvas,Rectangle,Color
+from kivy.graphics import Rectangle,Color
 from kivy.uix.checkbox import CheckBox
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
@@ -68,62 +72,20 @@ class uilayout(GridLayout):
         self.event_counter = 1
         self._cN = 1
         self.avoid_event = set(Clock.get_events())
+        self.mouse = Mcontrol()
+        self.keyboard = Kcontrol()
+        self.prev_time_k = [Clock.get_time(),None]
+
+        #Keyboard_lister
+        self.kboard = Window.request_keyboard(
+            self._keyboard_closed, self, 'text')
+        if self.kboard.widget:
+            # If it exists, this widget is a VKeyboard object which you can use
+            # to change the keyboard layout.
+            pass
+        self.kboard.bind(on_key_down=self._on_keyboard_down)
+
         Clock.schedule_once(self.settings1)
-
-
-
-    def start_event(self,*args):
-        self.canC()
-        self.force_stop = False
-        try:
-            interv = float(self._delay.text)
-        except ValueError:
-            self._delay.text = '0.5'
-            return
-        try:
-            delay = float(self._delay_on_start.text)
-        except ValueError:
-            self._delay_on_start.text = '1'
-            return
-        lim = int(self._limit.text)
-        if self.event_counter > lim:
-            self.event_counter = 1
-        def cycle(*args):
-            sv = self.event_counter
-            if sv >= lim+1:
-                self.restart(neg=False)
-                return False
-            if self.force_stop:
-                return False
-            self.dis1.text = str(sv)
-            self.dis3.text = str(round(sv/lim*100,2)) + '%'
-            self.Graphics(neg=False)
-            self.event_counter += 1
-        self._clear()
-        run_event = lambda *args:Clock.schedule_interval(cycle,interv)
-        _ = Clock.schedule_once(run_event,delay)
-
-    def canC(self,neg=True,*args):
-        if self.event_counter != self._cN and neg:
-            self.dis2.canvas.before.clear()
-
-    def _clear(self,*args):
-        for i in Clock.get_events():
-            if i not in self.avoid_event:
-                i.cancel()
-
-    def stop(self,*args):
-        self._clear()
-        self.force_stop = True
-
-    def restart(self,*args,**kwargs):
-        neg = kwargs.get('neg',True)
-        self.stop()
-        if neg:
-            self.dis1.text = '0'
-            self.dis3.text = '0%'
-        self.canC(neg=neg)
-        self.event_counter = 1
 
     def settings1(self,*args):
         self.avoid_event.update(Clock.get_events())
@@ -170,21 +132,91 @@ class uilayout(GridLayout):
         self._allow_enter_press = CheckBox(size_hint=[0.25, 0.25])
         self.SETTINGS2.add_widget(self._allow_enter_press)
 
+    def start_event(self,*args):
+        self.canC()
+        self.force_stop = False
+        try:
+            interv = float(self._delay.text)
+        except ValueError:
+            self._delay.text = '0.5'
+            return
+        try:
+            delay = float(self._delay_on_start.text)
+        except ValueError:
+            self._delay_on_start.text = '1'
+            return
+        lim = int(self._limit.text)
+        if self.event_counter > lim:
+            self.event_counter = 1
+        def cycle(*args):
+            sv = self.event_counter
+            if sv >= lim+1:
+                self.restart(neg=False)
+                return False
+            if self.force_stop:
+                return False
+            self.dis1.text = str(sv)
+            self.dis3.text = str(round(sv/lim*100,2)) + '%'
+            self.Graphics(neg=False)
+
+            #Main Command Hear
+            if self._allow_left_click.active:
+                self.mouse.press(MButton.left)
+                self.mouse.release(MButton.left)
+            if self._allow_space_bar.active:
+                self.keyboard.press(Key.space)
+                self.keyboard.release(Key.space)
+            if self._allow_right_click.active:
+                self.mouse.press(MButton.right)
+                self.mouse.release(MButton.right)
+            if self._allow_w_key.active:
+                self.keyboard.press('w')
+                self.keyboard.release('w')
+            if self._allow_enter_press.active:
+                self.keyboard.press(Key.enter)
+                self.keyboard.release(Key.enter)
+            self.event_counter += 1
+        self._clear()
+        run_event = lambda *args:Clock.schedule_interval(cycle,interv)
+        _ = Clock.schedule_once(run_event,delay)
+
+    def stop(self,*args):
+        self._clear()
+        self.force_stop = True
+
+    def restart(self,*args,**kwargs):
+        neg = kwargs.get('neg',True)
+        self.stop()
+        if neg:
+            self.dis1.text = '0'
+            self.dis3.text = '0%'
+        self.canC(neg=neg)
+        self.event_counter = 1
+
+    def canC(self,neg=True,*args):
+        if self.event_counter != self._cN and neg:
+            self.dis2.canvas.before.clear()
+
+    def _clear(self,*args):
+        for i in Clock.get_events():
+            if i not in self.avoid_event:
+                i.cancel()
 
     def Graphics(self,neg=True,*args):
         lim = int(self._limit.text)
         sec = self.event_counter
         self._cN = sec
+        sec-=1
         [X,Y,],[W,H] = self.dis2.pos,self.dis2.size
         w = W*0.85
         h = H/lim*0.85
         x = X+0.15/2*W
         rt = sec/lim
-        mp = math.pi/2.2
-        r = math.cos(rt*mp)
-        g = math.cos(rt*mp-mp)
-        b = math.cos(rt*mp-2*mp)
-        print(sec,r,g,b)
+        mp = math.pi/2.4
+        dx = math.pi/2*rt
+        r = math.cos(dx)
+        g = math.cos(dx-mp)
+        b = math.cos(dx-2*mp)
         try:
             y = Y+H*rt
         except ZeroDivisionError:
@@ -195,6 +227,37 @@ class uilayout(GridLayout):
             # Add a rectangle
             Rectangle(pos=[x,y],size=[w,h])
 
+    # kerboard listerner commands
+
+    def _keyboard_closed(self):
+        self.kboard.unbind(on_key_down=self._on_keyboard_down)
+        self.kboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+
+        k = keycode[1]
+        now = Clock.get_time()
+        prevk = self.prev_time_k[1]
+        dt = now - self.prev_time_k[0]
+
+        # Keycode is composed of an integer + a string
+        # If we hit escape, release the keyboard
+        if dt < 0.5:
+            if prevk == 'alt':
+                if k == 'f1':
+                    self.stop()
+                if k == 'f2':
+                    self.restart()
+                if k == 'f3':
+                    self.start_event()
+        if keycode[1] == 'escape':
+            keyboard.release()
+        # Return True to accept the key. Otherwise, it will be used by
+        # the system.
+        self.prev_time_k = [now,k]
+        return True
+
+    # end
 
 class AutoKeys(App):
     def build(self):
